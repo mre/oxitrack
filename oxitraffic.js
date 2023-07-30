dbf27753a33226fe34f470c754f908722e685c87
mo8it <mo8it@proton.me>
2023-07-30 03:44:11 +0200

    Improve shitty JS script

--- oxitraffic.js
+++ oxitraffic.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const oxitraffic_base_url = "https://oxitraffic.mo8it.com";
+const oxitraffic_base_url = "https://YOUR_OXITRAFFIC_URL";
 
-const registration_id = await fetch(oxitraffic_base_url + "/register?path=" + window.location.pathname).then((
-  response,
-) => response.json());
+const registration_response = await fetch(oxitraffic_base_url + "/register?path=" + window.location.pathname);
+const registration_id = await registration_response.json();
 await new Promise(resolve => setTimeout(() => resolve(), 15000));
 await fetch(oxitraffic_base_url + "/post-sleep/" + registration_id);
+console.log("Successful OxiTraffic report :)");
