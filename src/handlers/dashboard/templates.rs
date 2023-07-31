9e07d2ab09be96d4b98f5c58829a68e8fc1a1e7a
mo8it <mo8it@proton.me>
2023-07-31 22:01:57 +0200

    Rename templates

--- src/handlers/dashboard/templates.rs
+++ src/handlers/dashboard/templates.rs
@@ -2,14 +2,14 @@ use crate::handlers::base_template::Base;
 use askama::Template;
 
 #[derive(Template)]
-#[template(path = "dashboard/stats.html")]
+#[template(path = "stats.html")]
 pub struct Stats<'a> {
     pub base: Base<'a>,
     pub svg: String,
 }
 
 #[derive(Template)]
-#[template(path = "dashboard/index.html")]
+#[template(path = "index.html")]
 pub struct Index<'a> {
     pub base: Base<'a>,
     pub paths: Vec<String>,
