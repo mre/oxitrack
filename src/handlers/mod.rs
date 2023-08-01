d0fa22f3498a61d0fa10fe5f46d9b4f81fe7d4ff
mo8it <mo8it@proton.me>
2023-08-01 16:10:04 +0200

    Use if-let instead of match on Option

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -38,55 +38,51 @@ pub async fn register(
     .ctx(Status::Internal)
     .err_msg_lz(|| format!("Failed to run path query for path {path}!"))?;
 
-    let path_id = match path_id {
-        Some(Id { id }) => id,
-        None => {
-            let status = reqwest::get(state.tracked_url_from_path(path))
-                .await
-                .ctx(Status::NotFound)
-                .err_msg_lz(|| {
-                    format!("Failed to look up the path {path} on the tracked website!")
-                })?
-                .status();
+    let path_id = if let Some(Id { id }) = path_id {
+        id
+    } else {
+        let status = reqwest::get(state.tracked_url_from_path(path))
+            .await
+            .ctx(Status::NotFound)
+            .err_msg_lz(|| format!("Failed to look up the path {path} on the tracked website!"))?
+            .status();
 
-            if !status.is_success() {
-                return Err(RespErr::new(Status::NotFound)
-                    .err_msg(format!("Path {path} not found on tracked website!")));
-            }
+        if !status.is_success() {
+            return Err(RespErr::new(Status::NotFound)
+                .err_msg(format!("Path {path} not found on tracked website!")));
+        }
 
-            // There is a possible race condition here.
-            // If two requests to the same new path try to insert it at the same time,
-            // then only one insertion will be succussful.
-            // If the insertion fails because of the constraint, we will try to select.
-            let inserted_id = sqlx::query_as!(
-                Id,
-                "INSERT INTO paths(path) VALUES ($1)
+        // There is a possible race condition here.
+        // If two requests to the same new path try to insert it at the same time,
+        // then only one insertion will be succussful.
+        // If the insertion fails because of the constraint, we will try to select.
+        let inserted_id = sqlx::query_as!(
+            Id,
+            "INSERT INTO paths(path) VALUES ($1)
                 ON CONFLICT ON CONSTRAINT unique_path DO NOTHING
                 RETURNING id",
+            path
+        )
+        .fetch_optional(&*state.db)
+        .await
+        .ctx(Status::Internal)
+        .err_msg_lz(|| format!("Failed to insert path {path}!"))?;
+
+        if let Some(Id { id }) = inserted_id {
+            id
+        } else {
+            // Other request did insert the path first.
+            sqlx::query_as!(
+                Id,
+                "SELECT id FROM paths
+                        WHERE path = $1",
                 path
             )
-            .fetch_optional(&*state.db)
+            .fetch_one(&*state.db)
             .await
             .ctx(Status::Internal)
-            .err_msg_lz(|| format!("Failed to insert path {path}!"))?;
-
-            match inserted_id {
-                Some(id) => id.id,
-                None => {
-                    // Other request did insert the path first.
-                    sqlx::query_as!(
-                        Id,
-                        "SELECT id FROM paths
-                        WHERE path = $1",
-                        path
-                    )
-                    .fetch_one(&*state.db)
-                    .await
-                    .ctx(Status::Internal)
-                    .err_msg_lz(|| format!("Failed to insert path {path}!"))?
-                    .id
-                }
-            }
+            .err_msg_lz(|| format!("Failed to insert path {path}!"))?
+            .id
         }
     };
 
