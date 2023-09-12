88586ddff96adcd948a8c39d4884761fce7c971f
mo8it <mo8it@proton.me>
2023-09-12 18:16:13 +0200

    Add timestamp to cargo env vars

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() {
+    let timestamp = time::OffsetDateTime::now_utc().unix_timestamp();
+    println!("cargo:rustc-env=BUILD_TIMESTAMP={timestamp}");
+
     // trigger recompilation when a new migration is added
     println!("cargo:rerun-if-changed=migrations");
 }
