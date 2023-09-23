62779e31a74ad52d8084e12e87eb08fc88a3bfa5
mo8it <mo8it@proton.me>
2023-09-23 03:41:47 +0200

    Small changes to the count script

--- templates/count.js
+++ templates/count.js
@@ -1,19 +1,19 @@
 async function count() {
-  // Prevent running on testing instances like localhost.
+  // Prevent running on testing instances like localhost
   if (window.location.protocol !== "https:") {
     return;
   }
 
-  // Register and get an ID.
-  const registration_response = await fetch("{{ base_url }}/register?path=" + window.location.pathname);
-  const registration_id = await registration_response.json();
+  // Register and get an ID
+  const registration_resp = await fetch("{{ base_url }}/register?path=" + window.location.pathname);
+  const registration_id = await registration_resp.json();
 
-  // Sleep the required amount before being able to call `/post-sleep`.
-  await new Promise(resolve => setTimeout(resolve, parseInt("{{ sleep_secs }}") * 1000));
+  // Sleep the required amount before being able to call `/post-sleep`
+  await new Promise(r => setTimeout(r, 1000 * parseInt("{{ sleep_secs }}")));
 
-  // Call `/post-sleep` for the visit to be counted.
+  // Call `/post-sleep` for the visit to be counted
   await fetch("{{ base_url }}/post-sleep/" + registration_id, {
-    referrer: document.referrer,
+    referrer: document.referrer
   });
 }
 
