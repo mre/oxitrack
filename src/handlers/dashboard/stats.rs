3690b69cd808e460fef59e26ba13c1a70cdab7ad
mo8it <mo8it@proton.me>
2023-10-14 19:36:30 +0200

    Move over to only bar chart with data generated on the server

--- src/handlers/dashboard/stats.rs
+++ src/handlers/dashboard/stats.rs
@@ -5,61 +5,148 @@ use axum::{
 };
 use futures::TryStreamExt;
 use oxi_axum_helpers::{RespErr, RespErrCtx, RespErrExt, Status, TryIntoTemplResp};
+use serde::Serialize;
 use sqlx::{types::BigDecimal, PgPool};
+use time::format_description::well_known::Rfc3339;
 
 use crate::{
     handlers::{base_template::Base, queries::PathQuery},
     states::AppState,
 };
 
+#[derive(Serialize)]
+struct DataPoint {
+    x: String,
+    y: i64,
+}
+
 struct Visits {
-    timestamps_json: String,
-    len: usize,
-    min_chart_timestamp: i64,
-    max_chart_timestamp: i64,
+    chart_json_data: String,
+    chart_max_count: i64,
+    chart_date_trunc: &'static str,
+    first: String,
+    len: i64,
     per_day: f64,
     average_time_spent_secs: Option<BigDecimal>,
 }
 
 impl Visits {
     async fn build(pool: &PgPool, path_id: i64) -> Result<Self, RespErr> {
-        // Converted to ms timestamp.
-        let timestamps = sqlx::query!(
-            "SELECT timestamp FROM visits
-            WHERE path_id = $1
-            ORDER BY timestamp",
+        let len = sqlx::query!(
+            r#"SELECT COUNT(*) AS "count!" FROM visits
+            WHERE path_id = $1"#,
             path_id,
         )
-        .fetch(pool)
-        .map_ok(|row| 1000 * row.timestamp.unix_timestamp())
-        .try_collect::<Vec<_>>()
+        .fetch_one(pool)
         .await
         .ctx(Status::Internal)
-        .err_msg("History query failed!")?;
+        .err_msg("Failed to query the count of visits")?
+        .count;
 
-        let len = timestamps.len();
+        if len < 1 {
+            return Err(RespErr::new(Status::NotFound)
+                .user_msg("The requested path has no counted visits yet."));
+        }
 
-        let min_timestamp = *timestamps
-            .first()
-            .ctx(Status::NotFound)
-            .user_msg("The requested path has no counted visits yet.")?;
+        let first_visit = sqlx::query!(
+            "SELECT timestamp FROM visits
+            WHERE path_id = $1
+            ORDER BY timestamp
+            LIMIT 1",
+            path_id,
+        )
+        .fetch_one(pool)
+        .await
+        .ctx(Status::Internal)
+        .user_msg("Failed to query the first visit")?
+        .timestamp;
 
-        let now_ms = 1000 * time::OffsetDateTime::now_utc().unix_timestamp();
-        let ms_since_first_visit = now_ms - min_timestamp;
-        let visits_per_day = if ms_since_first_visit > MS_PER_DAY {
-            len as f64 * MS_PER_DAY_F / ms_since_first_visit as f64
+        let first_visit_formatted = first_visit
+            .format(&Rfc3339)
+            .ctx(Status::Internal)
+            .err_msg("Failed to format the datetime of the first visit!")?;
+
+        let now = time::OffsetDateTime::now_utc();
+        let days_since_first_visit = (now - first_visit).whole_days();
+        let visits_per_day = if days_since_first_visit > 0 {
+            len as f64 / days_since_first_visit as f64
         } else {
             len as f64
         };
 
-        let x_axis_padding = (ms_since_first_visit as f64 * 0.004) as i64;
-        let min_chart_timestamp = min_timestamp - x_axis_padding;
-        let max_chart_timestamp = now_ms + x_axis_padding;
+        let (chart_data, chart_date_trunc) = if days_since_first_visit > 1460 {
+            // More than 4 years.
+            let data = sqlx::query!(
+                r#"SELECT date_trunc('year', timestamp) AS "trunc_timestamp!",
+                COUNT(timestamp) AS "count!" FROM visits
+                WHERE path_id = $1
+                GROUP BY "trunc_timestamp!"
+                ORDER BY "trunc_timestamp!""#,
+                path_id,
+            )
+            .fetch(pool)
+            .map_ok(|row| DataPoint {
+                x: row.trunc_timestamp.year().to_string(),
+                y: row.count,
+            })
+            .try_collect::<Vec<_>>()
+            .await;
 
-        let timestamps_json = serde_json::to_string(&timestamps)
+            (data, "year")
+        } else if days_since_first_visit > 62 {
+            // More than 2 months.
+            let data = sqlx::query!(
+                r#"SELECT date_trunc('month', timestamp) AS "trunc_timestamp!",
+                COUNT(timestamp) AS "count!" FROM visits
+                WHERE path_id = $1
+                GROUP BY "trunc_timestamp!"
+                ORDER BY "trunc_timestamp!""#,
+                path_id,
+            )
+            .fetch(pool)
+            .map_ok(|row| DataPoint {
+                x: row.trunc_timestamp.month().to_string(),
+                y: row.count,
+            })
+            .try_collect::<Vec<_>>()
+            .await;
+
+            (data, "month")
+        } else {
+            let data = sqlx::query!(
+                r#"SELECT date_trunc('day', timestamp) AS "trunc_timestamp!",
+                COUNT(timestamp) AS "count!" FROM visits
+                WHERE path_id = $1
+                GROUP BY "trunc_timestamp!"
+                ORDER BY "trunc_timestamp!""#,
+                path_id,
+            )
+            .fetch(pool)
+            .map_ok(|row| DataPoint {
+                x: row.trunc_timestamp.date().to_string(),
+                y: row.count,
+            })
+            .try_collect::<Vec<_>>()
+            .await;
+
+            (data, "day")
+        };
+
+        let chart_data = chart_data
+            .ctx(Status::Internal)
+            .err_msg("Failed to query chart data!")?;
+
+        let chart_json_data = serde_json::to_string(&chart_data)
             .ctx(Status::Internal)
             .err_msg("Failed to convert history to JSON string!")?;
 
+        let chart_max_count = chart_data
+            .iter()
+            .map(|point| point.y)
+            .max()
+            .unwrap_or_default();
+
+        // TODO: In minutes instead of seconds.
         let average_time_spent_secs = sqlx::query!(
             "SELECT EXTRACT(EPOCH FROM AVG(left_at - timestamp)) FROM visits
             WHERE path_id = $1",
@@ -73,10 +160,11 @@ impl Visits {
         .map(|decimal| decimal.round(0));
 
         Ok(Self {
-            timestamps_json,
+            chart_json_data,
+            chart_max_count,
+            chart_date_trunc,
+            first: first_visit_formatted,
             len,
-            min_chart_timestamp,
-            max_chart_timestamp,
             per_day: visits_per_day,
             average_time_spent_secs,
         })
@@ -115,10 +203,6 @@ struct Stats<'a> {
     pub visits: Visits,
     pub referrers: Vec<Referrer>,
 }
-
-// Milliseconds per day.
-const MS_PER_DAY: i64 = 86_400_000;
-const MS_PER_DAY_F: f64 = MS_PER_DAY as f64;
 
 pub async fn get(
     State(state): AppState,
