3df71705e26f3e85632552a1af05f1e52d4b2b63
mo8it <mo8it@proton.me>
2023-11-01 16:12:53 +0100

    Don't show the UTC offset for verbose datetimes (especially first visit)

--- src/formatters/datetime_verbose.rs
+++ src/formatters/datetime_verbose.rs
@@ -7,11 +7,10 @@ impl fmt::Display for DateTimeVerboseFormatter {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "{} {:02}:{:02} UTC{}",
+            "{} {:02}:{:02}",
             self.0.date(),
             self.0.hour(),
             self.0.minute(),
-            self.0.offset(),
         )
     }
 }
