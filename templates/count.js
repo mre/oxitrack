eb93b9a616546faeba0be8ddebae798333ee866e
mo8it <mo8it@proton.me>
2023-11-18 23:52:26 +0100

    Report time on page while it is visible

--- templates/count.js
+++ templates/count.js
@@ -4,20 +4,32 @@ async function count() {
     return;
   }
 
+  let startTime = new Date();
+  let timeOnPageMs = 0;
+
   // Register and get an ID
-  const registration_resp = await fetch("{{ base_url }}/register?path=" + encodeURIComponent(window.location.pathname));
-  const visitor_id = await registration_resp.json();
+  const registrationResp = await fetch("{{ base_url }}/register?path=" + encodeURIComponent(window.location.pathname));
+  const visitorId = await registrationResp.json();
+
+  // Measure time only while the page is visible
+  document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "hidden") {
+      timeOnPageMs += new Date().getTime() - startTime.getTime();
+    } else {
+      startTime = new Date();
+    }
+  });
 
   // Sleep the required amount before being able to call `/post-sleep`
   await new Promise(r => setTimeout(r, 1000 * parseInt("{{ sleep_secs }}")));
 
   // Prepare query parameters
-  let query_params = "";
+  let queryParams = "";
   if (document.referrer.length > 0) {
     try {
       const referrer = new URL(document.referrer);
       if (referrer.protocol === "https:" && referrer.origin !== window.location.origin) {
-        query_params = "?referrer_origin=" + encodeURIComponent(referrer.origin);
+        queryParams = "?referrer_origin=" + encodeURIComponent(referrer.origin);
       }
     } catch (e) {
       console.log(e);
@@ -25,11 +37,13 @@ async function count() {
   }
 
   // Call `/post-sleep` for the visit to be counted
-  await fetch("{{ base_url }}/post-sleep/" + visitor_id + query_params);
+  await fetch("{{ base_url }}/post-sleep/" + visitorId + queryParams);
 
-  // Call `/page-left` to record the total spent time
+  // Call `/page-left` to report the total spent time
   window.addEventListener("beforeunload", async () => {
-    await fetch("{{ base_url }}/page-left/" + visitor_id);
+    timeOnPageMs += new Date().getTime() - startTime.getTime();
+    const timeOnPageS = Math.round(0.001 * timeOnPageMs);
+    await fetch("{{ base_url }}/page-left/" + visitorId + "/" + timeOnPageS);
   });
 }
 
