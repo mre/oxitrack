36f12f13ff6991574c7b910d5abdf619345b6d46
mo8it <mo8it@proton.me>
2025-04-04 16:58:19 +0200

    Simplify dashboard index

--- src/handlers/dashboard/index.rs
+++ src/handlers/dashboard/index.rs
@@ -1,18 +1,18 @@
 use askama::Template;
 use askama_web::WebTemplate;
-use axum::{extract::State, response::IntoResponse};
+use axum::extract::State;
 
 use crate::{handlers::base_template::Base, states::AppState};
 
 #[derive(Template, WebTemplate)]
 #[template(path = "index.html")]
-struct Index<'a> {
-    pub base: Base<'a>,
+pub struct Index {
+    pub base: Base<'static>,
     pub base_url: &'static str,
     pub tracked_origin: &'static str,
 }
 
-pub async fn get(State(state): AppState) -> impl IntoResponse {
+pub async fn get(State(state): AppState) -> Index {
     Index {
         base: Base::new(state, "Dashboard"),
         base_url: state.base_url,
