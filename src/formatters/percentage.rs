c0ad88915647ec3134e3528abdbdfa7f74256ea5
Matthias <matthias@endler.dev>
2026-07-16 16:31:23 +0200

    Fix share bar percentage widths

--- src/formatters/percentage.rs
+++ src/formatters/percentage.rs
@@ -4,6 +4,6 @@ pub struct PercentageFormatter(pub f64);
 
 impl fmt::Display for PercentageFormatter {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:.1}", self.0)
+        write!(f, "{:.1}%", self.0)
     }
 }
